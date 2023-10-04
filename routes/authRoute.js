98f412e434c2bdfb64df6555507527e6610c2492
Atish Chandole <auto91akc@gmail.com>
2023-10-04 21:11:14 +0530

    Add gitignore file

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,11 +7,11 @@ const router = express.Router()
 router.post('/register',registerController)
 router.post('/login',loginController)
 router.get('/test/token',requireSignIn,isAdmin,testTokenController)
-router.get('/forgot-password',forgotController)
+router.post('/forgot-password',forgotController)
 
 //Protected route
 router.get('/user-auth',requireSignIn,(req,res)=>{
     res.status(201).send({ok:true})
-})
+}) 
 
 export default router
