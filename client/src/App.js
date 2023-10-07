93c5a974e2222f5654c2bc29d6c1f7906ab95d8f
Atish Chandole <auto91akc@gmail.com>
2023-10-07 23:05:45 +0530

    User and Admin panel UI

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,11 @@ import PrivateRoute from './components/Routes/Private'
 import ForgotPassword from './pages/Auth/ForgotPassword'
 import AdminRoute from './components/Routes/Admin'
 import AdminDashboard from './pages/Admin/AdminDashboard'
+import CreateCategory from './pages/Admin/CreateCategory'
+import CreateProduct from './pages/Admin/CreateProduct'
+import Users from './pages/Admin/Users'
+import Orders from './pages/User/Orders'
+import Profile from './pages/User/Profile'
 
 function App() {
   return (
@@ -18,10 +23,15 @@ function App() {
       <Routes>
       <Route path='/' element={<HomePage/>} />
       <Route path='/dashboard' element={<PrivateRoute/>}>
-        <Route path='user' element={<Dashboard/>} />         
+        <Route path='user' element={<Dashboard/>} />
+        <Route path='user/orders' element={<Orders/>} />
+        <Route path='user/profile' element={<Profile/>} />
       </Route>
       <Route path='/dashboard' element={<AdminRoute/>} >
         <Route path='admin' element={<AdminDashboard/>} />
+        <Route path='admin/create-category' element={<CreateCategory/>} />
+        <Route path='admin/create-product' element={<CreateProduct/>} />
+        <Route path='admin/users' element={<Users/>} />
       </Route>
       <Route path='/about' element={<About/>} />
       <Route path='/contact' element={<Contact/>} />
